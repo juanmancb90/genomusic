1d17b02e1e3f653f1adbc1f4d8bc08dc2bfae702
andres-root <andres.root.coder@gmail.com>
2014-11-23 00:06:13 -0500

    added scripts and styles

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,32 @@
 var Walker = function() {  
-    this.x = window.innerWidth / 2
-    this.y = window.innerHeight / 2
-    this.radius = 1
+    this.x = Math.floor(Math.random() * (window.innerWidth - 100 - window.innerWidth - 100 + 1)) + window.innerWidth - 100;
+    this.y = window.innerHeight - 100
+    this.radius = 5
     this.frameRate = 1000/30
     this.canvas = document.getElementById('canvas')
     this.context = this.canvas.getContext('2d')
-    this.canvas.width = window.innerWidth
-    this.canvas.height = window.innerHeight
+    this.canvas.width = window.innerWidth - 100
+    this.canvas.height = window.innerHeight - 100
 }
 
 Walker.prototype.setup = function() {
+    this.context.clearRect(0, 0, canvas.width, canvas.height);
     this.context.beginPath()
     this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false)
-    this.context.fillStyle = 'black'
+    this.context.fillStyle = 'white'
     this.context.fill()
 }
 
 Walker.prototype.walk = function() {
     r = Math.random()
-    if (r < 0.1) {
-        this.x++
-    } else if (r < 0.2) {
-        this.x--
-    } else if (r < 0.4) {
-        this.y++
-    } else {
-        this.y--
-    }
+    this.y--
 }
 
 Walker.prototype.animate = function() {
-    this.walk()
-    this.setup()
+    setInterval(function(){this.walk(); this.setup();}, this.frameRate)
 }
 
 var run = function(){
-    w = new Walker()
-    setInterval(function(){w.animate()}, w.frameRate)
+    walker = new Walker()
+    walker.animate()
 }
-run()
