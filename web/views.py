7d05e28d04755c507aa371040a5e35d790aa02b8
andres-root <andres.root.coder@gmail.com>
2014-11-23 01:09:31 -0500

    cleaning and merging

--- web/views.py
+++ web/views.py
@@ -1,20 +1,14 @@
+# Django Modules
 from django.shortcuts import render
+
+# App Modules
+from sequencer.utils import search
 
 
 def index(request):
     context = {}
     return render(request, 'web/index.html', context)
 
+
 def get_protein(request):
-    """
-    tomar query_key y webenv:
-
-    http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi/?db=<PROTEIN o NUCLEOTIDE>&term=<NOMBRE DE LA PROTEINA>&retmax=1&usehistory=y
-    eutils.ncbi.nlm.nih.gov
-    eutils.ncbi.nlm.nih.gov
-
-
-
-    http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=<PROTEIN o NUCLEOTIDE>&query_key=<QUERY_KEY>&WebEnv=<WEBENV>&rettype=
-fasta&retmode=text&retmax=1
-    """
+    pass
