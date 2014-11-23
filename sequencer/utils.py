2d1864aa1ff37ee4b1766bfb52d5f2a8640db449
andres-root <andres.root.coder@gmail.com>
2014-11-23 02:00:00 -0500

    added scripts to make web search

--- sequencer/utils.py
+++ sequencer/utils.py
@@ -8,11 +8,10 @@ def get_sequence(db, query_key, web_env):
     terms = '?db={0}&query_key={1}&WebEnv={2}&rettype=fasta&retmode=text&retmax=1'.format(db, query_key, web_env)
     url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/{0}'.format(terms)
     response = urllib.request.urlopen(url)
-    return response.read()
+    return response.readlines()
 
 
-def search(term):
-    db = 'nucleotide'
+def search(term, db):
     terms = '?db={0}&term={1}&retmax=1&usehistory=y'.format(db, term)
     url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi/{0}'.format(terms)
     response = requests.get(url)
@@ -25,4 +24,5 @@ def search(term):
 if __name__ == '__main__':
     print('Organism Name: \n')
     term = input()
-    print(search(term))
+    db = 'nucleotid'
+    print(search(term, db))
--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'web.views',
-    url(r'^', 'index', name='index')
+    url(r'^', 'index', name='index'),
+    url(r'^sequence/get/', 'get_sequence', name='get_sequence')
 )
