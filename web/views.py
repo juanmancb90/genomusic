96cc71b986c327b09577b32c75bbfe95709de266
andres-root <andres.root.coder@gmail.com>
2014-11-23 00:11:05 -0500

    added get nucleotid funcion

--- web/views.py
+++ web/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'web/index.html', context)
+
+def get_protein(request):
+    """
+     query_key y webenv:
+
+    http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi/?db=<PROTEIN o NUCLEOTIDE>&term=<NOMBRE DE LA PROTEINA>&retmax=1&usehistory=y
+    eutils.ncbi.nlm.nih.gov
+    eutils.ncbi.nlm.nih.gov
+    """
+    pass
