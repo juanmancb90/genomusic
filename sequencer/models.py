e1320f8d0f454f2e32b0f0e63ee4fbcd45005ac2
andres-root <andres.root.coder@gmail.com>
2014-11-23 01:18:55 -0500

    added sequence model

--- sequencer/models.py
+++ sequencer/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Sequence(models.Model):
+    """ Sequence Model
+    """
+    DB_CHOICES = (
+        ('protein', 'protein'),
+        ('nucleotide', 'nucleotide'),
+    )
+    name = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    db = models.CharField(
+        max_length=500,
+        choices=DB_CHOICES,
+        null=True,
+        blank=True
+    )
+    query_key = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    web_env = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    header = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    raw = models.TextField()
+    active = models.BooleanField(default=True)
--- web/views.py
+++ web/views.py
@@ -10,5 +10,5 @@ def index(request):
     return render(request, 'web/index.html', context)
 
 
-def get_protein(request):
+def search_sequence(request):
     pass
