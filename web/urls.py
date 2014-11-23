bcd046d56e87976f20ae33399558122b245f5966
andres-root <andres.root.coder@gmail.com>
2014-11-23 02:52:14 -0500

    heroku settings

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import *
 
 urlpatterns = patterns(
-    'web.views',
+    'genomusic.web.views',
     url(r'^$', 'index', name='index'),
     url(r'^sequence/search/$', 'search_sequence', name='search_sequence')
 )
